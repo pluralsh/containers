ac73db68c064100f67317924e909a826c95aae14
David van der Spek <vanderspek.david@gmail.com>
2023-04-06 11:18:21 +0200

    test with keeping upstream image tags
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>

--- release.config.js
+++ release.config.js
@@ -1,8 +1,9 @@
 const name = process.env.APP_NAME;
+const tag = process.env.IMAGE_TAG;
 
 module.exports = {
   branches: ["main"],
-  tagFormat: name + '-v${version}',
+  tagFormat: name + '-' + tag + '-plural' + '-v${version}',
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
