2065e40ca32b952d75d9b38d6182c5d1f45c192c
David van der Spek <vanderspek.david@gmail.com>
2023-04-06 11:39:00 +0200

    add test workflow for building container
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>

--- release.config.js
+++ release.config.js
@@ -1,9 +1,8 @@
 const name = process.env.APP_NAME;
-const tag = process.env.IMAGE_TAG;
 
 module.exports = {
   branches: ["main"],
-  tagFormat: name + '-' + tag + '-plural' + '-v${version}',
+  tagFormat: name + '-v${version}',
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
